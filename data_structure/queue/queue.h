c9d280020db21f16667188c994b4477de86e0ceb
frost <frost@ncuhome.cn>
2016-10-15 21:30:09 +0800

    Queue

---
+++ data_structure/queue/queue.h
@@ -0,0 +1,17 @@
+#ifndef DATA_STRUCTURE_QUEUE_H
+#define DATA_STRUCTURE_QUEUE_H
+
+template<class T>
+class Queue
+{
+ public:
+  virtual ~Queue() { }
+  virtual bool empty() const = 0;
+  virtual int size() const = 0;
+  virtual T& front() = 0; // 返回头元素的引用
+  virtual T& back() = 0; // 返回尾元素的引用
+  virtual void pop() = 0; // 删除首元素
+  virtual void push(const T& element) const = 0; // 把元素element加入队尾
+};
+
+#endif
