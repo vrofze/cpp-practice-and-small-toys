2f80003ccc63af00322f664db6904f76e8e60e25
frost <frost@ncuhome.cn>
2016-10-23 22:25:23 +0800

    MaxHeap

--- data_structure/tree/max_heap_main.cc
+++ data_structure/tree/max_heap_main.cc
@@ -5,9 +5,20 @@
 int main()
 {
   MaxHeap<int> heap;
-  int arr[] = {0, 20, 12, 35, 15, 10, 80, 30, 17, 2, 1};
-  std::cout << "here" << std::endl;
-  heap.initialize(arr, 11);
-  std::cout << "here" << std::endl;
-  // std::cout << heap.pop() << std::endl;
+  int arr[] = {20, 12, 35, 15, 10, 80, 30, 17, 2, 1};
+  heap.initialize(arr, 10);
+  std::cout << heap << std::endl;
+  std::cout << heap.pop() << std::endl;
+  std::cout << heap << std::endl;
+  heap.push(90);
+  heap.push(90);
+  heap.push(100);
+  heap.push(90);
+  std::cout << heap << std::endl;
+  std::cout << heap.pop() << std::endl;
+  std::cout << heap.pop() << std::endl;
+  std::cout << heap.pop() << std::endl;
+  std::cout << heap.pop() << std::endl;
+  std::cout << heap.pop() << std::endl;
+  std::cout << heap << std::endl;
 }
