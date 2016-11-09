6f95b0ef7cdbcdeadcb6dbbac9247a6a2bdbb425
vrofze <vrofze@gmail.com>
2016-11-09 22:44:11 +0800

    Graph

---
+++ data_structure/graph/graph.h
@@ -0,0 +1,24 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+template<class T>
+class graph
+{
+public:
+    virtual ~graph() { }
+
+    virtual int num_of_vertices() const = 0;
+    virtual int num_of_edges() const = 0;
+    virtual bool exists_edge(int, int) const = 0;
+    virtual void insert_edge(edge<T>*) = 0;
+    virtual void erase_edge(int, int) = 0;
+    virtual int degree(int) const = 0;
+    virtual int in_degree(int) const = 0;
+    virtual int out_degree(int) const = 0;
+
+    virtual bool directed() const = 0;
+    virtual bool weighted() const = 0;
+    virtual vertex_iterator<T>* iterator(int) = 0;
+};
+
+#endif
