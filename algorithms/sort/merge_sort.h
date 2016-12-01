863008d63542c3b36fe38a51eff203c5fdcbe54f
vrofze <vrofze@gmail.com>
2016-12-01 21:47:18 +0800

    Promise

---
+++ algorithms/sort/merge_sort.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+template<typename T>
+void merge_sort(T *arr, size_t length)
+{
+    
+}
+
+template<typename T>
+void merge(T *arr, size_t q, size_t p, size_t r)
+{
+    
+}
