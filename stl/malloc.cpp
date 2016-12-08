26f4a90d8061567399ad8bd6e9079707f382b3c1
vrofze <vrofze@gmail.com>
2016-12-08 22:54:35 +0800

    Malloc

--- stl/malloc.cpp
+++ stl/malloc.cpp
@@ -2,21 +2,36 @@
 #include <iostream>
 #include <new>
 
+#define output std::cout << "haha" << std::endl; std::cout << "haha again" << std::endl;
+
 int main()
 {
-    void *p = malloc(sizeof(int));
-    std::cout << "sizeof p:" << sizeof(p) << std::endl;
-    new(p) int(10);
-    std::cout << p << std::endl;
+    void *p1 = malloc(1);
+    void *p2 = malloc(1);
+    std::cout << "sizeof p1:" << sizeof(p1) << std::endl;
+    new(p1) int(1234567);
+    std::cout << *(int *)p1 << std::endl;
+    std::cout << "sizeof p2:" << sizeof(p2) << std::endl;
+    std::cout << *(int *)p2 << std::endl;
 
-    free(p);
+    free(p1);
 
-    p = 0;
+    p1 = 0;
 
-    free(p);
+    free(p1);
 
-    p = new int();
-    std::cout << *(int *)p << std::endl;
+    p1 = new int();
+    std::cout << *(int *)p2 << std::endl;
+
+    output;
+
+    enum Day {Fri = 100, The, Thu};
+    enum {Fr = 8, Se};
+
+    Day day1= The;
+
+    std::cout << day1 << std::endl;
+    std::cout << Fr << std::endl;
 
     return 0;
 }
